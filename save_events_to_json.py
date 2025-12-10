3c97cc754a03dda74922ce8d29c33b6497d7b31c
lifeoverthinker <96396847+lifeoverthinker@users.noreply.github.com>
2025-12-10 13:52:06 +0100

    remove event scraper

--- save_events_to_json.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-from wydarzenia_scraper import parse_events, URL
-
-if __name__ == "__main__":
-    html = requests.get(URL).text
-    events = parse_events(html)
-    with open("events.json", "w", encoding="utf-8") as f:
-        json.dump(events, f, ensure_ascii=False, indent=2)
